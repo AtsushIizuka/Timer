2c648f071cdc33eb894a2ab665b1ed01b3a9323c
EC2 Default User <ec2-user@ip-172-31-13-244.us-west-2.compute.internal>
2022-09-12 07:16:04 +0000

    firstcommit

---
+++ taimer.js
@@ -0,0 +1,59 @@
+const timeElement = document.getElementById("time");
+
+const start= document.getElementById("start") ;
+
+const stop = document.getElementById("stop") ;
+
+const reset = document.getElementById("reset") ;
+
+let elapsed = 0;
+
+let intervalID = null ;
+
+function updateTime() {
+    const ms = elapsed % 1000;
+    const s = Math.floor(elapsed / 1000);
+    const m =Math.floor(elapsed / (1000 * 60));
+    const h =Math.floor(elapsed / (1000*60*60));
+    
+    const msStr = ms.toString().padStart(2,'0');
+    const sStr = s.toString().padStart(2,'0');
+    const mStr = m.toString().padStart(2,'0');
+    const hStr = h.toString().padStart(2,'0');
+    
+    timeElement.innerHTML = `${hStr} : ${mStr} : ${sStr}.${msStr} `;
+   
+}
+
+document.getElementById("stop").disabled = true;
+document.getElementById("reset").disabled = true;
+
+start.addEventListener("click", function(e) {
+    document.getElementById("stop").disabled = false;
+    document.getElementById("reset").disabled = false;
+    document.getElementById("start").disabled = true;
+    if (intervalID !== null){ return;}
+  let pre = new Date();
+  intervalID = setInterval(function() {
+      const now = new Date();
+      elapsed += now - pre;
+      pre = now ;
+      updateTime()
+      //console.log(elapsed);
+  },10);
+});
+
+
+stop.addEventListener("click", function(e) {
+    document.getElementById("start").disabled = false;
+    document.getElementById("stop").disabled = true;
+  clearInterval(intervalID) ; 
+  intervalID = null;
+});
+
+
+reset.addEventListener("click", function(e) {
+    document.getElementById("stop").disabled = false;
+   elapsed = 0 ; 
+   updateTime();
+});
